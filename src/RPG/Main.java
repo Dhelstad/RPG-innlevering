72faaf33fdf568bb709da2c0981fe68000d86a13
“Daniel <helstad59@hotmail.com>
2022-12-21 10:00:40 +0100

    updated main with every charachter so the stats and dps is shown.

--- src/RPG/Main.java
+++ src/RPG/Main.java
@@ -1,6 +1,9 @@
 package RPG;
 
 
+import RPG.Characters.Mage;
+import RPG.Characters.Ranger;
+import RPG.Characters.Rogue;
 import RPG.Characters.Warrior;
 import RPG.Item.Armor;
 import RPG.Item.Slot;
@@ -20,6 +23,46 @@ public class Main {
 
 
         thor.display();
+
+        System.out.println("this is a separation line.");
+
+        Mage Odin = new Mage("Odin") ;
+
+        Weapon staff = new Weapon("Staff", 1, Slot.WEAPON, Weapon.Type.STAFF,3, 2.5);
+        Odin.Equip(staff);
+
+        Armor cloth = new Armor("CLOTH", 1, Slot.BODY, Armor.Type.CLOTH, 8, 1, 3);
+        Odin.Equip(cloth);
+
+
+        Odin.display();
+
+        System.out.println("this is a separation line.");
+
+        Rogue Loke = new Rogue("Loke") ;
+
+        Weapon dagger = new Weapon("Dagger", 1, Slot.WEAPON, Weapon.Type.DAGGER,2, 4.5);
+        Loke.Equip(dagger);
+
+        Armor mail = new Armor("MAIL", 1, Slot.BODY, Armor.Type.MAIL, 7, 1, 2);
+         Loke.Equip(mail);
+
+
+        Loke.display();
+
+
+        System.out.println("this is a separation line.");
+
+        Ranger Balder = new Ranger("Balder") ;
+
+        Weapon bow = new Weapon("BOW", 1, Slot.WEAPON, Weapon.Type.BOW,1, 2.5);
+        Balder.Equip(bow);
+
+        Armor leather = new Armor("LEATHER", 1, Slot.BODY, Armor.Type.LEATHER, 5, 2, 1);
+        Balder.Equip(leather);
+
+
+        Balder.display();
 
     }
 }
